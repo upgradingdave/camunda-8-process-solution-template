e5d48a191f4a2c49c7dc08ad0cedb6e8bb1fa53b
Dave Paroulek <david.paroulek@camunda.com>
2026-04-23 19:46:16 -0400

    Removed some hard coded variables

--- frontend/src/keycloak.js
+++ frontend/src/keycloak.js
@@ -1,9 +1,9 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'https://dave01.gke.c8sm.com/auth',
-  realm: 'camunda-platform',
-  clientId: 'camunda-react-app',
+  url: import.meta.env.VITE_KEYCLOAK_URL,
+  realm: import.meta.env.VITE_KEYCLOAK_REALM,
+  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
 });
 
 export default keycloak;
