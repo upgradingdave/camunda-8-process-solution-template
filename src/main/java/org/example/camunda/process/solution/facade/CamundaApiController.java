d2dadbeb40a5ef88d3c15f48fc58da2400550842
Dave Paroulek <david.paroulek@camunda.com>
2026-04-23 17:10:48 -0400

    Added orchestration api facade

---
+++ src/main/java/org/example/camunda/process/solution/facade/CamundaApiController.java
@@ -0,0 +1,33 @@
+package org.example.camunda.process.solution.facade;
+
+import io.camunda.client.CamundaClient;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CamundaApiController {
+
+  private static final Logger LOG = LoggerFactory.getLogger(CamundaApiController.class);
+  private final CamundaClient camundaClient;
+
+  public CamundaApiController(CamundaClient client) {
+    this.camundaClient = client;
+  }
+
+  @GetMapping("/camunda/userTasks")
+  public List<?> getUserTasksByAssignee(@RequestParam String assignee) {
+    LOG.info("Fetching user tasks assigned to: {}", assignee);
+    return camundaClient
+        .newUserTaskSearchRequest()
+        .filter(f -> f.assignee(assignee))
+        .send()
+        .join()
+        .items();
+  }
+}
